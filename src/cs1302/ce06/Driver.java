110f1a3c944258325ef903c40e5264f9bff89774
Brad Barnes <barnes@nike.cs.uga.edu>
2019-01-23 15:26:15 -0500

    Updated source code

--- src/cs1302/ce06/Driver.java
+++ src/cs1302/ce06/Driver.java
@@ -8,31 +8,55 @@ import cs1302.ce06.impl.Car;
  * {@code cs1302.ce06.contract.Drivable}
  */
 public class Driver {
+
+    /**
+     * Attempts to speedup and slowdown the {@link cs1302.ce06.impl.Car} object
+     * by a specified amount. If the {@link cs1302.ce06.impl.Car} object is unable
+     * to speedup or slowdown by the specified amount, an appropriate message is
+     * printed.
+     *
+     * @param car the car to test
+     * @param speedupAmount the amount to speedup the car
+     * @param slowdownAmount the amount to slowdown the car
+     */ 
+    public static void test(Car car, double speedupAmount,
+			    double slowdownAmount) {
+	System.out.println(car);
+	if(car.speedUp(speedupAmount)) {
+	    System.out.println("The car sped up by " + speedupAmount + " mph");
+	} else {
+	    System.out.println("The car cannot go that fast");
+	} // if
+
+	if(car.slowDown(slowdownAmount)) {
+	    System.out.println("The car slowed down by " + slowdownAmount + " mph");
+	} else {
+	    System.out.println("The car is cannot slow down by that amount");
+	} // if
+
+	System.out.println(car);
+    } // test
+
+    /**
+     * 
+     * @param args the command-line arguments to the program
+     */
     public static void main(String[] args) {
 	Car fastCar = new Car(185.5);
 	Car slowCar = new Car(75.0);
 
-	System.out.println(slowCar);
-	System.out.println(fastCar);
-
-	if(slowCar.speedUp(20.0)) {
-	    System.out.println("slowCar sped up");
-	} // if
-	if(fastCar.speedUp(75.5)) {
-	    System.out.println("fastCar sped up");
-	} // if
+	System.out.println("--------------------");
 	
-	slowCar.speedUp(80.5);
-	fastCar.speedUp(200);
+	test(fastCar, 200.5, 20);
+	System.out.println("--------------------");
 
-	if(slowCar.slowDown(10)) {
-	    System.out.println("slowCar slowed down");
-	} // if
-	if(fastCar.slowDown(25.0)) {
-	    System.out.println("fastCar slowed down");
-	} // if
-
-	System.out.println(slowCar);
-	System.out.println(fastCar);   
+	test(fastCar, 125, 20);
+	System.out.println("--------------------");
+	
+	test(slowCar, 85, 20);
+	System.out.println("--------------------");
+	
+	test(slowCar, 65, 65);
+	System.out.println("--------------------");
     } // main
 } // Driver
